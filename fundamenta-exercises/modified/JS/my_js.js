c3d1b1a29d3519f07e542778e0e3c8533fdef7f6
Miguel C <miguelcudaihl@gmail.com>
2016-01-24 07:07:05 -0800

    Finished exercises 1-3

--- fundamenta-exercises/modified/JS/my_js.js
+++ fundamenta-exercises/modified/JS/my_js.js
@@ -1,18 +1,28 @@
 // 05 Context
-DOT = function(obj, prop){
-
-}
+var DOT = function(obj, prop){
+    if(obj.hasOwnProperty(prop)) {
+            return obj[prop];
+        }
+        else {
+            return DOT(obj.__proto__, prop);
+        }
+    }
 
 // 05 Context
-DOTCALL = function(obj, prop, args){
+var DOTCALL = function(obj, prop, args){
+    var fn = DOT(obj, prop);
+    
+    if(typeof fn === 'function') {
+        return fn.apply(obj, args);
+    }
 
 }
 
 // 06 Prototypes
-NEW = function(constructor, args){
+var NEW = function(constructor, args){
 
 }
 
-INSTANCEOF = function(obj, constructor){
+var INSTANCEOF = function(obj, constructor){
 
 }
