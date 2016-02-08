f4aebbf6fd594bbe782cca59a5620a03a456bcaa
Rene Miguel Cudaihl <Rene Miguel Cudaihl>
2016-02-09 00:38:07 +0800

    Finished exercise 16

--- fundamenta-exercises/modified/DOM/my_jquery.js
+++ fundamenta-exercises/modified/DOM/my_jquery.js
@@ -1,22 +1,16 @@
 (function () {
     $ = function (selector) {
         
-        if(!(this instanceof $)) {
+        // Use this in case NEW is not used
+        if (!(this instanceof $)) {
             return new $(selector);
         }
-        
-        var r = document.querySelectorAll(selector);
-        //this.length = r.length;
-        // //$.extend(self, new Array());
-        // 
-        // self.length = r.length;
-        // $.each(r, function(index, value){
-        //     self[index] = value;
-        // });
-       
-        Array.prototype.push.apply(this, r);
-        
-        
+
+        if (!(selector instanceof Array)) {
+            selector = document.querySelectorAll(selector);
+        }
+
+        Array.prototype.push.apply(this, selector);
     };
 
     $.extend = function (target, object) {
@@ -25,6 +19,25 @@
         }
         return target;
     };
+
+    var getText = function (el) {
+        var txt = '';
+
+        if (el.nodeType === 3) {
+            txt += el.nodeValue;
+        }
+
+        $.each(el.childNodes, function (i, childNode) {
+            //getText.call(this, childNode);
+            if (childNode.nodeType === 3) {
+                txt += childNode.nodeValue;
+            } else if (childNode.nodeType === 1) {
+                txt += getText(childNode);
+            }
+        });
+
+        return txt;
+    }
 
     // Static methods
     var isArrayLike = function (obj) {
@@ -61,7 +74,7 @@
             return newArr;
         },
         proxy: function (fn, context) {
-            return function () { 
+            return function () {
                 return fn.apply(context, arguments);
             };
         }
@@ -69,35 +82,104 @@
 
     $.extend($.prototype, {
         html: function (newHtml) {
-            if(arguments.length===0){
+            if (arguments.length === 0) {
                 return this[0].innerHTML;
-            } 
-            else{
-               $.each(this, function(i, el){
-                  el.innerHTML = newHtml; 
-               });
-               
-               return this;
+            }
+            else {
+                $.each(this, function (i, el) {
+                    el.innerHTML = newHtml;
+                });
+
+                return this;
             }
         },
         val: function (newVal) {
-             if(arguments.length===0){
+            if (arguments.length === 0) {
                 return this[0].value;
-            } 
-            else{
-               $.each(this, function(i, el){
-                  el.value= newVal;
-               });
-               
-               return this;
+            }
+            else {
+                $.each(this, function (i, el) {
+                    el.value = newVal;
+                });
+
+                return this;
             }
         },
-        text: function (newText) { },
-        find: function (selector) { },
-        next: function () { },
+        text: function (newText) {
+            if (arguments.length) {
+                $.each(this, function (i, el) {
+                    el.innerHTML = "";
+                    el.appendChild(document.createTextNode(newText));
+                });
+
+                return this;
+            }
+            else {
+                // var returnTxt = '';
+                // 
+                // $.each(this, function(i, el){
+                //       returnTxt += el.textContent;
+                // })
+                // 
+                // return returnTxt;
+                
+                return this[0] && getText.call(this[0], this[0]);
+            }
+        },
+        find: function (selector) {
+            var accumulator = [];
+
+            $.each(this, function (i, el) {
+                var r = el.querySelectorAll(selector);
+
+                [].push.apply(accumulator, r)
+            });
+
+            return $(accumulator);
+        },
+        next: function () {
+            var accumulator = [];
+
+            $.each(this, function (i, el) {
+                var current = el.nextSibling;
+
+                while (current && current.nodeType != Node.ELEMENT_NODE) {
+                    current = current.nextSibling;
+                }
+
+                if (current) {
+                    accumulator.push(current);
+                }
+
+            });
+
+            return $(accumulator);
+
+        },
         prev: function () { },
-        parent: function () { },
-        children: function () { },
+        parent: function () {
+            var accumulator = [];
+            $.each(this, function (i, el) {
+               var r = el.parentNode;
+
+                [].push.apply(accumulator, r)
+            });
+
+            return $(accumulator); 
+            
+        },
+        children: function () {
+            var accumulator = [];
+
+            $.each(this, function (i, el) {
+                var r = el.childNodes;
+
+                [].push.apply(accumulator, r)
+            });
+
+            return $(accumulator);
+
+        },
         attr: function (attrName, value) { },
         css: function (cssPropName, value) { },
         width: function () { },
